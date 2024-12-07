c7cec0cefa9817ec2f38793c9852b5a09206fd49
Gabriela Menezes <gabimenezess10@gmail.com>
2024-12-07 15:12:03 -0300

    feat: add theme provider to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <Header />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   )
 }
