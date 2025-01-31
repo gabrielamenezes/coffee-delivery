17c0d01f3cae4fa509bba6dd62f0cc87ab36d174
Gabriela Menezes <gabimenezess10@gmail.com>
2025-01-31 19:47:20 -0300

    feat: wrap logo in NavLink for improved navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,25 @@
-import { MapPin, ShoppingCart } from '@phosphor-icons/react'
-import { CartButtonContainer, HeaderContainer } from './style'
-import logoCoffeeDelivery from '/images/logo.svg'
-import { NavLink } from 'react-router-dom'
+import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import { CartButtonContainer, HeaderContainer } from "./style";
+import logoCoffeeDelivery from "/images/logo.svg";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
-
     <HeaderContainer>
-      <img src={logoCoffeeDelivery} alt="Lodo do Coffee Delivery" />
+      <NavLink to={"/"}>
+        <img src={logoCoffeeDelivery} alt="Lodo do Coffee Delivery" />
+      </NavLink>
       <nav>
         <div>
           <MapPin weight="fill" />
           Belo Horizonte, MG
         </div>
-        <NavLink to={"/cart"} title='Carrinho de compras'>
+        <NavLink to={"/cart"} title="Carrinho de compras">
           <CartButtonContainer name="Carrinho de compras">
             <ShoppingCart weight="fill" />
           </CartButtonContainer>
         </NavLink>
       </nav>
     </HeaderContainer>
-  )
-}
+  );
+};
